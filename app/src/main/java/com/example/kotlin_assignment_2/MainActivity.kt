c8afde795c1dc0ccb8179d70e7e98e01c037dd26
noamguy <noam.guy09@gmail.com>
2026-01-27 01:10:48 +0200

    assignment 3

--- app/src/main/java/com/example/kotlin_assignment_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_assignment_2/MainActivity.kt
@@ -1,48 +1,32 @@
 package com.example.kotlin_assignment_2
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.kotlin_assignment_2.databinding.ActivityMainBinding
-import com.example.kotlin_assignment_2.models.Model
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: StudentsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.studentListRv.layoutManager = LinearLayoutManager(this)
-
-        val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        binding.studentListRv.addItemDecoration(itemDecoration)
-
-        adapter = StudentsAdapter(Model.students)
-        binding.studentListRv.adapter = adapter
-
-        adapter.onRowClickListener = { position ->
-            val intent = Intent(this, StudentDetailsActivity::class.java)
-            intent.putExtra(StudentDetailsActivity.EXTRA_STUDENT_ID, Model.students[position].id)
-            startActivity(intent)
-        }
-
-        adapter.onCheckboxClickListener = { position, isChecked ->
-            Model.students[position].isPresent = isChecked
-        }
-
-        binding.mainAddStudentBtn.setOnClickListener {
-            startActivity(Intent(this, NewStudentActivity::class.java))
-        }
+        
+        setSupportActionBar(binding.toolbar)
+        
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
-
-    override fun onResume() {
-        super.onResume()
-        adapter.updateData(Model.students)
+    
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
